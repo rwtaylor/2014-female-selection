e0bec5f59ce435c1ac89c2b756a9bb29d66d90a2
Ryan W. Taylor <ryan@ryantaylor.net>
2014-07-16 16:45:24 -0700

    Script to install R packages

---
+++ rpackages.R
@@ -0,0 +1,23 @@
+# Source this file to install all required packages (that aren't already installed.)
+
+installed.pkgs <- installed.packages()[ ,"Package"]
+
+# CRAN packages
+required.pkgs <- c("devtools", "MCMCglmm","lme4", "foreach", "doMC", "psych", "ggplot2", "GGally", "ggthemes", "knitr", "pander", "mvnormtest", "car", "grid", "effects")
+
+
+new.pkgs <- required.pkgs[!required.pkgs %in% installed.pkgs]
+
+if(length(new.pkgs)){
+  install.packages(new.pkgs)
+}
+
+# Github packages
+if(!any(installed.pkgs == "dplyr")){
+  devtools::install_github("hadley/dplyr")
+}
+
+if(!any(installed.pkgs == "tidyr")){
+  devtools::install_github("hadley/tidyr")
+}
+
